c2388dab3b1641acce7165d96629fcda3ffd3ff5
雪饼 <wangxuejin@tutudeAir.fios-router.home>
2020-04-26 18:19:28 -0400

    Add map and sightings apps to settinga

--- Squirrel/settings.py
+++ Squirrel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*q1*s-3!_ewi90s!v8s56_ne*^nc$&=dit$5!n+nvijwblfyrj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'map',
+    'sightings',
 ]
 
 MIDDLEWARE = [
