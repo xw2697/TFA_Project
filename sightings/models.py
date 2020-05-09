2674d85af3bc465ae7165ecd2f80e917a7dc69fb
Xuejin Wang <xw2697@columbia.edu>
2020-05-08 23:20:21 -0400

    Add models.py in sightings

--- sightings/models.py
+++ sightings/models.py
@@ -15,7 +15,7 @@ class Squirrel(models.Model):
     longitude = models.FloatField('Longitude')
     unique_Squirrel_ID = models.CharField('Unique_Squirrel_ID',max_length=50)
     shift = models.CharField('Shift',max_length=50, choices=CHOICES_SHIFT)
-    date = models.dateField('Date')
+    date = models.DateField('Date')
     age = models.CharField('Age',max_length=50, choices=CHOICES_AGE)
 from django.db import models
 
