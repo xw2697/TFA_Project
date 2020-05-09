598dcc30993e808acabc3e0132c9db79637f848b
Xuejin Wang <xw2697@columbia.edu>
2020-05-08 23:08:14 -0400

    Add models.py to sightings

--- sightings/models.py
+++ sightings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Squirrel(models.Model):
+    CHOICES_SHIFT = (
+        ('am','AM'),
+        ('pm','PM'),
+    )
+    CHOICES_AGE = (
+        ('adult','Adult'),
+        ('juvenile','Juvenile'),
+    )
+
+    latitude = models.FloatField('Latitude')
+    longitude = models.FloatField('Longitude')
+    unique_Squirrel_ID = models.CharField('Unique_Squirrel_ID',max_length=50)
+    shift = models.CharField('Shift',max_length=50, choices=CHOICES_SHIFT)
+    date = models.dateField('Date')
+    age = models.CharField('Age',max_length=50, choices=CHOICES_AGE)
+from django.db import models
+
+# Create your models here.
--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('Hello TUTU')
+
+
+# Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
